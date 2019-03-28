44f5ebfe3d8330c3c847f8f7803f00d92a4a6f2a
Renovamen <renovamenzxh@gmail.com>
2019-03-28 21:57:53 +0800

    [Add] speech to text - baidu api and iflytek api

--- input.py
+++ input.py
@@ -1,8 +1,8 @@
-from TextSpeech import speech_to_text
+from TextSpeech import speech_to_text_baidu
 
 def get_input():
     print("---- please say something and type enter ----")
-    return speech_to_text()
+    return speech_to_text_baidu()
     #return _input_from_command()
 
 def _input_from_command():
